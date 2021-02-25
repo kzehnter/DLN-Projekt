3ec40006d8dcdd5de8c6610e8dceb0789d2be29a
decimo1 <konstantinzehnter@protonmail.com>
2021-02-25 13:37:16 +0100

    arduino secrets and gitignore

--- dln_project.ino
+++ dln_project.ino
@@ -2,6 +2,7 @@
 #include <WiFi101.h>
 #include "epd4in2.h"
 #include "epdpaint.h"
+#include "arduino_secrets.h"
 
 #define COLORED     0
 #define UNCOLORED   1
@@ -9,29 +10,23 @@
 
 static char buf[BUFSIZE+1];
 
-// WLAN
-const char ssid[] = "WLAN-10.";
-const char pass[] = "}&Xt34DmMq/<*d-M";
-
 // Telegram
 WiFiSSLClient client;
 const char url[] = "api.telegram.org";
-String botEndPoint = "/bot1411748234:AAEpr0T2dvKmEYfsniMDOI8iQmFUi0rGvK0/getUpdates?offset=-1";
 
 // Display
 Epd display;
-const char testText[] = "Hallo Welt und DLN-Kurs! Das hier ist ein Test";
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
   
   if (!connectWiFi()) return;
-  //const char text[] = getText()
-  //if (getText() == NULL) return;
-  const char *rawMessage = getText(); 
-  Serial.println(rawMessage); 
-  //if (!writeOnDisplay(rawMessage)) return;
+  char *rawMessage = getText(); 
+  if (rawMessage == NULL) return;
+  Serial.println(rawMessage);
+  char *message = convertText(rawMessage);
+  //if (writeOnDisplay(const_cast<char*>(rawMessage)) == 1) return;
 }
 
 void loop() {
@@ -57,8 +52,8 @@ bool connectWiFi(){
 /** 
  * 
  */
-bool writeOnDisplay(const char text[]){
-  if (display.Init() != 0) return false;              //init display with reset
+int writeOnDisplay(const char text[]){
+  if (display.Init() != 0) return 1;              //init display with reset
   display.ClearFrame();                               //clear SRAM of display
   unsigned char image[1500];                          //create small image buffer  
   int height = 24;                                    //different for font sizes
@@ -69,19 +64,22 @@ bool writeOnDisplay(const char text[]){
   display.SetPartialWindow(paint.GetImage(), 0, 0 /*adjust to go down a row*/, paint.GetWidth(), paint.GetHeight());
   display.DisplayFrame();                             //refresh display -> shows image
   display.Sleep();                                    //display back in deep sleep
-  return true;
+  return 0;
 }
 
+/**
+ * 
+ */
 char * getText(){
-  if (!client.connect(url, 443)) return ""; 
+  if (!client.connect(url, 443)) return NULL; 
   Serial.println("Connected");
-  client.println("GET " + botEndPoint);
+  client.print("GET ");
+  client.println(botEndPoint);
   client.println("Content-Type: application/json");
   client.println();
 
   
   int j = 0;
-  bool lastEOL = false;
   while (j++ != 50) {
     if (client.available()) {
       int i = 0;
@@ -98,4 +96,11 @@ char * getText(){
   client.println("Connection: close");
   client.stop();
   return buf;
+}
+
+/**
+ * 
+ */
+char * convertText(char text[]) {
+  return text;
 }
